2587d76f588255d74b8a9dbdc0a41add18c93b7c
Ian Johnson <tacoda@pm.me>
2018-12-26 21:52:27 -0500

    Add mutations

--- hngql/schema.py
+++ hngql/schema.py
@@ -1,7 +1,13 @@
 import graphene
 import links.schema
 
+
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
